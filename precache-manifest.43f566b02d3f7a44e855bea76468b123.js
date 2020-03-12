516b6e5962866ae5a92b4d5a4129085d168e7129
BlowITA <brblowgames@gmail.com>
2020-03-12 01:41:10 -0300

    Updates

---
+++ precache-manifest.43f566b02d3f7a44e855bea76468b123.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e44ecd9b8dac688ea4a07cfd6bda5f47",
+    "url": "/poe-cluster-jewel-rating/index.html"
+  },
+  {
+    "revision": "35b64bea876d29aef508",
+    "url": "/poe-cluster-jewel-rating/static/js/2.57c48b7c.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/poe-cluster-jewel-rating/static/js/2.57c48b7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e59dc6ed3a556a54e306",
+    "url": "/poe-cluster-jewel-rating/static/js/main.7b0a0f96.chunk.js"
+  },
+  {
+    "revision": "2db00b426c3ec06b0120",
+    "url": "/poe-cluster-jewel-rating/static/js/runtime-main.51c535b9.js"
+  }
+]);
